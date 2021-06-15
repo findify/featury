1f19d4979251d19cc2060ff9687da5c2e0cc3434
Roman Grebennikov <grv@dfdx.me>
2021-06-15 11:04:22 +0300

    add logging to API

--- api/src/main/scala/io/findify/featury/api/ValuesApi.scala
+++ api/src/main/scala/io/findify/featury/api/ValuesApi.scala
@@ -8,15 +8,21 @@ import io.findify.featury.values.FeatureStore
 import org.http4s.circe.{jsonEncoderOf, jsonOf}
 import io.circe.syntax._
 import org.http4s.circe._
+import org.typelevel.log4cats.Logger
 
-case class ValuesApi(store: FeatureStore) {
+case class ValuesApi(store: FeatureStore, logger: Logger[IO]) {
   import ValuesApi._
   val service = HttpRoutes.of[IO] {
     case GET -> Root / "status" => Ok("")
     case post @ POST -> Root / "api" / "values" =>
       for {
-        read     <- post.as[ReadRequest]
+        read <- post.as[ReadRequest]
+        _ <- logger.debug(
+          s"received request: ns=${read.ns} scope=${read.scope} tenant=${read.tenant} features=${read.features
+            .map(_.value)} ids=${read.ids.map(_.value)}"
+        )
         response <- store.read(read)
+        _        <- logger.debug(s"read ${response.features.size} values")
         ok       <- Ok(response.asJson)
       } yield {
         ok
